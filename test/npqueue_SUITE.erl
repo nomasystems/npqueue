ca9018e9d7948e70d1d68b9d613f06372b77e82c
tak30 <pablolvi@ext.zara.com>
2022-04-27 12:16:54 +0200

    improving repo before publication

--- test/npqueue_SUITE.erl
+++ test/npqueue_SUITE.erl
@@ -1,4 +1,4 @@
-%%% Copyright 2021 Nomasystems, S.L. http://www.nomasystems.com
+%%% Copyright 2022 Nomasystems, S.L. http://www.nomasystems.com
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -13,17 +13,8 @@
 %% limitations under the License.
 -module(npqueue_SUITE).
 
-%%% INCLUDE FILES
--include_lib("common_test/include/ct.hrl").
-
 %%% EXTERNAL EXPORTS
--compile(export_all).
-
-%%% MACROS
--define(MATCH_SPEC, [{'_', [], [{message, {return_trace}}]}]).
--define(MAX_TIME, 10000).
-% Tests run in log/ct_run.*
--define(STUBS_DIR, "../../stubs").
+-compile([export_all, nowarn_export_all]).
 
 %%%-----------------------------------------------------------------------------
 %%% SUITE EXPORTS
@@ -39,78 +30,33 @@ all() ->
         performance
     ].
 
-sequences() ->
-    [].
-
-suite() ->
-    [{timetrap, {minutes, 60}}].
-
 %%%-----------------------------------------------------------------------------
 %%% INIT SUITE EXPORTS
 %%%-----------------------------------------------------------------------------
 init_per_suite(Conf) ->
-    lists:foreach(fun(X) -> code:add_path(X) end, ct:get_config(paths, [])),
-    dbg:tracer(),
-    dbg:p(all, [c, sos, sol, timestamp]),
-    Apps = ct:get_config(apps, []),
-    Env = ct:get_config(env, []),
-    [ok = application:load(App) || App <- Apps],
-    [ok = application:set_env(App, K, V) || {App, KeyVal} <- Env, {K, V} <- KeyVal],
-    [ok = application:start(App) || App <- Apps],
-    Conf.
+    nct_util:setup_suite(Conf).
 
 %%%-----------------------------------------------------------------------------
 %%% END SUITE EXPORTS
 %%%-----------------------------------------------------------------------------
-end_per_suite(_Conf) ->
-    Apps = ct:get_config(apps, []),
-    [ok = application:stop(App) || App <- Apps],
-    [ok = application:unload(App) || App <- Apps],
-    ok.
+end_per_suite(Conf) ->
+    nct_util:teardown_suite(Conf).
 
 %%%-----------------------------------------------------------------------------
 %%% INIT CASE EXPORTS
 %%%-----------------------------------------------------------------------------
 init_per_testcase(Case, Conf) ->
     ct:print("Starting test case ~p", [Case]),
-    init_stubs(Case),
-    init_traces(Case),
+    nct_util:init_traces(Case),
     Conf.
-
-init_stubs(Case) ->
-    NegCases = ct:get_config(neg_cases, []),
-    Stubs = proplists:get_value(Case, NegCases, []),
-    lists:foreach(fun(Stub) -> load_stub(Stub, true) end, Stubs).
-
-init_traces(Case) ->
-    TpCases = ct:get_config(tp_cases, []),
-    Tps = proplists:get_value(Case, TpCases, []),
-    lists:foreach(fun(Tp) -> add_trace(tp, Tp) end, Tps),
-    TplCases = ct:get_config(tpl_cases, []),
-    Tpls = proplists:get_value(Case, TplCases, []),
-    lists:foreach(fun(Tpl) -> add_trace(tpl, Tpl) end, Tpls).
 
 %%%-----------------------------------------------------------------------------
 %%% END CASE EXPORTS
 %%%-----------------------------------------------------------------------------
 end_per_testcase(Case, Conf) ->
-    end_traces(Case),
-    end_stubs(Case),
+    nct_util:end_traces(Case),
     ct:print("Test case ~p completed", [Case]),
     Conf.
-
-end_stubs(Case) ->
-    NegCases = ct:get_config(neg_cases, []),
-    Stubs = proplists:get_value(Case, NegCases, []),
-    lists:foreach(fun purge_stub/1, Stubs).
-
-end_traces(Case) ->
-    TpCases = ct:get_config(tp_cases, []),
-    Tps = proplists:get_value(Case, TpCases, []),
-    lists:foreach(fun(Tp) -> del_trace(ctp, Tp) end, Tps),
-    TplCases = ct:get_config(tpl_cases, []),
-    Tpls = proplists:get_value(Case, TplCases, []),
-    lists:foreach(fun(Tpl) -> del_trace(ctpl, Tpl) end, Tpls).
 
 %%%-----------------------------------------------------------------------------
 %%% TEST CASES
@@ -131,16 +77,13 @@ api(_Conf) ->
     ConsumerCount = 5,
     CountIn = PartitionCount * 10,
     ProducerIn = fun(X) -> npqueue:in(Name, X) end,
-    {ok, QueuePid} = npqueue:start_link(Name, PartitionCount, ConsumerCount, ConsumerFun),
+    {ok, _QueuePid} = npqueue:start_link(Name, PartitionCount, ConsumerCount, ConsumerFun),
     true = npqueue:is_empty(Name),
     0 = npqueue:len(Name),
     0 = npqueue:total_in(Name),
     0 = npqueue:total_out(Name),
     Rps = 1000,
     ok = npqueue:rps(Name, Rps),
-    State = sys:get_state(QueuePid),
-    io:format("Queue state: ~p~n", [State]),
-    ct:print("Queue state: ~p~n", [State]),
     lists:foreach(ProducerIn, lists:seq(1, CountIn)),
     CountIn = npqueue:total_in(Name),
     false = npqueue:is_empty(Name),
@@ -169,16 +112,13 @@ force_order_using_partitions(_Conf) ->
     ProducerIn = fun(X) ->
         npqueue:in(Name, X, PartitionSelector)
     end,
-    {ok, QueuePid} = npqueue:start_link(Name, PartitionCount, ConsumerCount, ConsumerFun),
+    {ok, _QueuePid} = npqueue:start_link(Name, PartitionCount, ConsumerCount, ConsumerFun),
     true = npqueue:is_empty(Name),
     0 = npqueue:len(Name),
     0 = npqueue:total_in(Name),
     0 = npqueue:total_out(Name),
     Rps = 10000,
     ok = npqueue:rps(Name, Rps),
-    State = sys:get_state(QueuePid),
-    io:format("Queue state: ~p~n", [State]),
-    ct:print("Queue state: ~p~n", [State]),
     lists:foreach(ProducerIn, lists:seq(1, CountIn)),
     CountIn = npqueue:total_in(Name),
     false = npqueue:is_empty(Name),
@@ -451,104 +391,6 @@ wait_true(Fun) ->
             timer:sleep(1000),
             wait_true(Fun)
     end.
-
-min_max_med_sum(L) ->
-    {Min, Max, Sum} = min_max_sum(L),
-    Med = lists:nth(length(L) div 2, lists:sort(fun({_, X}, {_, Y}) -> X < Y end, L)),
-    {Min, Max, element(2, Med), Sum}.
-
-min_max_sum(L) ->
-    min_max_sum(L, 0, 0, 0).
-
-min_max_sum([], Min, Max, Sum) ->
-    {Min, Max, Sum};
-min_max_sum([{_, H} | T], Min, Max, Sum) when H < Min ->
-    min_max_sum(T, H, Max, Sum + H);
-min_max_sum([{_, H} | T], Min, Max, Sum) when H > Max ->
-    min_max_sum(T, Min, H, Sum + H);
-min_max_sum([{_, H} | T], Min, Max, Sum) ->
-    min_max_sum(T, Min, Max, Sum + H).
-
-%%%-----------------------------------------------------------------------------
-%%% TRACING UTIL FUNCTIONS
-%%%-----------------------------------------------------------------------------
-add_trace(TpFun, {Mod, Fun, Spec}) ->
-    dbg:TpFun(Mod, Fun, Spec);
-add_trace(TpFun, {Mod, Fun}) ->
-    dbg:TpFun(Mod, Fun, ?MATCH_SPEC);
-add_trace(TpFun, Mod) ->
-    dbg:TpFun(Mod, ?MATCH_SPEC).
-
-del_trace(CtpFun, {Mod, Fun, _Spec}) ->
-    dbg:CtpFun(Mod, Fun);
-del_trace(CtpFun, {Mod, Fun}) ->
-    dbg:CtpFun(Mod, Fun);
-del_trace(CtpFun, Mod) ->
-    dbg:CtpFun(Mod).
-
-%%%-----------------------------------------------------------------------------
-%%% STUB UTIL FUNCTIONS
-%%%-----------------------------------------------------------------------------
-load_stub(Stub, NegTest) ->
-    Opts =
-        if
-            NegTest -> [binary, {d, neg_case}];
-            true -> [binary]
-        end,
-    Erl = atom_to_list(Stub) ++ ".erl",
-    ct:print("Compiling ~s with options ~p", [Erl, Opts]),
-    {ok, Mod, Bin} = compile:file(filename:join(?STUBS_DIR, Erl), Opts),
-    ct:print("Purge default ~p stub", [Mod]),
-    code:purge(Mod),
-    code:delete(Mod),
-    ct:print("Loading new ~p stub", [Mod]),
-    Beam = atom_to_list(Mod) ++ code:objfile_extension(),
-    {module, Mod} = code:load_binary(Mod, Beam, Bin).
-
-purge_stub(Stub) ->
-    ct:print("Purge ~p stub", [Stub]),
-    code:purge(Stub),
-    code:delete(Stub),
-    ct:print("Reloading default ~p stub", [Stub]),
-    {module, Stub} = code:load_file(Stub).
-
-%%%-----------------------------------------------------------------------------
-%%% INTERNAL FUNCTIONS
-%%%-----------------------------------------------------------------------------
-date_time_after(DateTime, Seconds) ->
-    DateTimeSeconds = calendar:datetime_to_gregorian_seconds(DateTime),
-    calendar:gregorian_seconds_to_datetime(DateTimeSeconds + Seconds).
-
-print_state() ->
-    ct:print("Status: ~p", [ets:tab2list(summary)]),
-    print_items().
-
-print_items() ->
-    print_items(ets:tab2list(items), 1000).
-
-print_items(_, 0) ->
-    ok;
-print_items(L, N) ->
-    TakeN = fun
-        ({X, Item}, {ItemsN, Rest}) when X == N ->
-            {[Item | ItemsN], Rest};
-        (X, {ItemsN, Rest}) ->
-            {ItemsN, [X | Rest]}
-    end,
-    {ItemsN, Rest} = lists:foldl(TakeN, {[], []}, L),
-    MaxItems = ct:get_config(items, 1000),
-    case length(ItemsN) of
-        Length when Length < MaxItems ->
-            Missing = lists:subtract(lists:seq(1, MaxItems), lists:reverse(ItemsN)),
-            ct:print("Missing ~p items from producer ~p: ~p", [MaxItems - Length, N, Missing]);
-        _ ->
-            ok
-    end,
-    print_items(Rest, N - 1).
-
-rand_bool() ->
-    R = rand:uniform(),
-    R < 0.5.
 
 times(0, _Fun) ->
     ok;
