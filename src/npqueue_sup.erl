fbd7113035fe8b3198c43006d3ec5630fce0eed6
Pablo López <tak30@users.noreply.github.com>
2022-05-25 11:33:57 +0200

    Update src/npqueue_sup.erl
    Co-authored-by: Javier Garea <javigarea@gmail.com>

--- src/npqueue_sup.erl
+++ src/npqueue_sup.erl
@@ -39,7 +39,7 @@ stop(QueueName) ->
     QueueNameBin = erlang:atom_to_binary(QueueName, utf8),
     case erlang:whereis(?SUP_NAME(QueueNameBin)) of
         undefined ->
-            {error, not_found};
+            {error, {not_found, QueueName}};
         Pid ->
             erlang:exit(Pid, shutdown)
     end.
